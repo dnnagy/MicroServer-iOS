c9b366be213e8844a006ff32d23640f1bf9eefd5
Nagy Dániel <nagy@chromeshift.com>
2018-09-04 17:19:38 +0200

    Initial Commit

---
+++ MicroServer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MicroServer
+//
+//  Created by Nagy Daniel on 2018. 09. 04..
+//  Copyright © 2018. Nagy Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
